abac42d386aff6310eb4dc195075855c20832a82
USER <USER@192.168.0.194>
2020-08-18 04:31:24 +0200

    Actividad 2

--- Camilo/src/me/Camilo/tutorial/Ejercicio5.java
+++
@@ -1,7 +0,0 @@
-package me.Camilo.tutorial;
-
-public class Ejercicio5 {
-	public double getCalories(double mass, double deltaTemp) {
-		return mass * deltaTemp;
-	}
-}
--- Camilo/src/me/Camilo/tutorial/Ejercicio6.java
+++
@@ -1,7 +0,0 @@
-package me.Camilo.tutorial;
-
-public class Ejercicio6 {
-	public double areaDePolygono(double n, double s) {
-		return ((n * Math.pow(s, 2)) / (4 * Math.tan((Math.PI)/n)));
-	}
-}
